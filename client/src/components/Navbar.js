f1c036eebff4725d41d5c8c4f6fc9bcc9d252c51
navneetkaur992002-cyber <navneetkaur992002@gmail.com>
2026-04-29 21:20:08 +0530

    mobile responsive update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,74 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/Greenscape.png";
 
-
 function Navbar() {
   return (
-    <nav style={{display:"flex", justifyContent:"space-between", alignItems:"center", padding:"15px", background:"#22671b"}}>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexWrap: "wrap",
+        padding: "15px",
+        background: "#22671b",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <img
+          src={logo}
+          alt="logo"
+          style={{
+            width: "80px",
+            height: "80px",
+            marginRight: "10px",
+          }}
+        />
 
-      <div style={{display:"flex", alignItems:"center"}}>
-        <img src={logo} alt="logo" style={{width:"100px",height:"100px", marginRight:"10px"}} />
-        <h2 style={{color:"white"}}>Greenscape Works</h2>
+        <h2
+          style={{
+            color: "white",
+            fontSize: "24px",
+            margin: 0,
+          }}
+        >
+          Greenscape Works
+        </h2>
       </div>
 
-      <div>
-        <Link to="/" style={{margin:"10px", color:"white"}}>Home</Link>
-        <Link to="/services" style={{margin:"10px", color:"white"}}>Services</Link>
-        <Link to="/gallery" style={{margin:"10px", color:"white"}}>Gallery</Link>
-        <Link to="/about" style={{margin:"10px", color:"white"}}>About</Link>
-        <Link to="/contact" style={{margin:"10px", color:"white"}}>Contact</Link>
-      
-      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        <Link to="/" style={{ margin: "8px", color: "white", textDecoration: "none" }}>
+          Home
+        </Link>
 
+        <Link to="/services" style={{ margin: "8px", color: "white", textDecoration: "none" }}>
+          Services
+        </Link>
+
+        <Link to="/gallery" style={{ margin: "8px", color: "white", textDecoration: "none" }}>
+          Gallery
+        </Link>
+
+        <Link to="/about" style={{ margin: "8px", color: "white", textDecoration: "none" }}>
+          About
+        </Link>
+
+        <Link to="/contact" style={{ margin: "8px", color: "white", textDecoration: "none" }}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
